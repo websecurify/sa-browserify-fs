96a3ae6800fb63b4e826ea21f72fd46009d7f877
Petko D. Petkov <pdp@gnucitizen.org>
2018-07-08 23:01:37 +0100

    Ignore Safari.

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const config = require('./config')
 
 if (typeof(document) === 'undefined') {
     module.exports = require('ws-browserify-fs')
-} else
-if (document.location.origin === config.remote) {
-    module.exports = require('ws-browserify-fs')
-
-    document.domain = config.hostname
 } else {
-    module.exports = require('./lib')
+    const isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1
+    
+    if (isSafari) {
+        module.exports = require('ws-browserify-fs')
+
+        document.domain = config.hostname
+    } else
+    if (document.location.origin === config.remote) {
+        module.exports = require('ws-browserify-fs')
+
+        document.domain = config.hostname
+    } else {
+        module.exports = require('./lib')
+    }
 }
