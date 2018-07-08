24c6ffe0fcbc4905b6331a2f93dc95ecba16f2f3
Petko D. Petkov <pdp@gnucitizen.org>
2018-07-08 23:26:28 +0100

    Dont fiddle with the domain.

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ if (typeof(document) === 'undefined') {
     module.exports = require('ws-browserify-fs')
 } else {
     const isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1
-    
+
     if (isSafari) {
         module.exports = require('ws-browserify-fs')
-
-        document.domain = config.hostname
     } else
     if (document.location.origin === config.remote) {
         module.exports = require('ws-browserify-fs')
